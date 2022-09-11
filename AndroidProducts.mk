95aa18cd2e50a52adbbf7a5be33c6550b2c893b3
Lafactorial <hknhzl@gmail.com>
2022-09-11 16:18:46 +0300

    Welcome Evolution X

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,5 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_tissot.mk
+    $(LOCAL_DIR)/evolution_tissot.mk
+
---
+++ evolution_tissot.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2017-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+EVO_BUILD_TYPE := UNOFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+
+# Inherit from tissot device
+$(call inherit-product, device/xiaomi/tissot/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := tissot
+PRODUCT_NAME := evolution_tissot
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi A1
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# LineageOS Stuffs
+TARGET_SUPPORTS_QUICK_TAP := true
