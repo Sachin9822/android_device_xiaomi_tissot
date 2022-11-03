f5b777ae8581ea97fd4b7fa642ae06db04ff719c
intervigilium <intervigil@gmail.com>
2022-11-03 14:01:13 +0530

    tissot: Better ram Management

--- prop.mk
+++ prop.mk
@@ -10,3 +10,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
 persist.camera.dual.camera=0
 
+# Better Ram
+ro.config.fha_enable=true
+ro.sys.fw.bg_apps_limit=32
+ro.config.dha_cached_max=16
+ro.config.dha_empty_max=42
+ro.config.dha_empty_init=32
+ro.config.dha_lmk_scale=0.545
+ro.config.dha_th_rate=2.3
+ro.config.sdha_apps_bg_max=64
+ro.config.sdha_apps_bg_min=8
