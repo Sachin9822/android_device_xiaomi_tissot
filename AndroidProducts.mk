1fdf785f5cb6a89522bf9e1fbbd4b787c0559a08
Sachin9822 <70788804+Sachin9822@users.noreply.github.com>
2022-12-20 17:07:34 +0000

    Cipher Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,5 +14,5 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/blaze_tissot.mk
+    $(LOCAL_DIR)/cipher_tissot.mk
 
--- cipher_tissot.mk
+++ cipher_tissot.mk
@@ -8,22 +8,23 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common ProjectBlaze stuff
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
-BLAZE_BUILD_TYPE := UNOFFICIAL
-BLAZE_MAINTAINER := Sachin
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USE_PIXEL_CHARGER := true
+# Inherit some common CipherOS stuff.
+$(call inherit-product, vendor/cipher/config/common_full_phone.mk)
 
-#gapps
-WITH_GAPPS := true
+# Inherit some common CipherOS flags
+CIPHER_OFFICIAL := false
+CIPHER_GAPPS := true
+CIPHER_MAINTAINER := Sachin
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+
 
 # Inherit from tissot device
 $(call inherit-product, device/xiaomi/tissot/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
-PRODUCT_NAME := blaze_tissot
+PRODUCT_NAME := cipher_tissot
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A1
@@ -31,6 +32,9 @@ PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+
+
 
 # LineageOS Stuffs
 TARGET_SUPPORTS_QUICK_TAP := true
